61d82e1ffadca2ff0e7d2a46cf4ffcd5daa4da5a
Anthony Donayre <e.a.donayre95@gmail.com>
2023-05-23 11:20:39 -0500

    se agrega contador e interaciion con stock simulado

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -2,8 +2,15 @@
 
 import './ItemDetail.css'
 import ItemCount from '../ItemCount/ItemCount'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
-const ItemDetail = ({ id, nombre, precio, img }) => {
+const ItemDetail = ({ id, nombre, precio, img, stock }) => {
+  const [agregarCantidad, setAgregarCantidad] = useState(0);
+  const manejadorCantidad = (cantidad) => {
+    setAgregarCantidad(cantidad);
+    console.log("Productos agregados: " + cantidad);
+  }
   return (
     <div className='contenedorItem'>
       <div className='descripContenedor'>
@@ -11,9 +18,10 @@ const ItemDetail = ({ id, nombre, precio, img }) => {
         <h3>Precio: s/ {precio} </h3>
         <h3>ID: {id} </h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus nihil quia, culpa iste quam velit? Ipsum amet, similique illum quam possimus voluptatum, nisi atque modi nemo, corporis minima consequatur maiores! Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut deleniti, veniam in animi deserunt, fugit fuga necessitatibus sapiente totam doloremque mollitia laborum illum sequi non atque, inventore consequatur qui omnis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Et eligendi sequi reprehenderit labore eaque quibusdam laboriosam quaerat consequuntur ad? Voluptates accusantium corporis hic unde architecto porro eveniet dolorum commodi qui?</p>
-        <ItemCount/>
+        {/* <ItemCount/> */}
       </div>
       <img src={img} alt={nombre} />
+      {agregarCantidad > 0 ? (<Link to='/cart'>Terminar compra</Link>) : (<ItemCount inicial={1} stock={stock} funcionAgregar={manejadorCantidad}/>)}
     </div>
   )
 }

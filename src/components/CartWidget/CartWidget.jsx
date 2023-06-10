c4a619c25797ae7d8b9f97cb9db57e527907ba7e
Anthony Donayre <e.a.donayre95@gmail.com>
2023-06-10 10:42:14 -0500

    modificacion estilos de body y nav

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,16 +1,16 @@
 import { useContext } from 'react';
-import { CarritoContext, carritoContext } from '../../context/carritoContext';
+import { CarritoContext } from '../../context/carritoContext';
 import { Link } from 'react-router-dom';
 import './CartWidget.css'
+import { Icon } from '@iconify/react';
 
 const CartWidget = () => {
   const { cantidadTotal } = useContext(CarritoContext);
-  const imgCart = "https://cdn-icons-png.flaticon.com/512/116/116356.png";
 
   return (
     <div>
       <Link to='/cart'>
-        <img className='imgCarrito' src={imgCart} alt="Carrito de Compras" />
+        <Icon icon="fluent:cart-16-filled" color="#ffd369" className='imgCarrito'/>
         {cantidadTotal > 0 && <span> {cantidadTotal} </span>}
       </Link>
     </div>

a15dc06a4b59aa660710520eeb1164f93c24e2a8
Anthony Donayre <e.a.donayre95@gmail.com>
2023-06-17 10:24:07 -0500

    cart, contador y precios actualizados-ok

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,11 +1,11 @@
-import { CarritoContext } from "../../context/carritoContext"
-import './Cart.css'
-import { useContext } from "react"
-import { Link } from "react-router-dom"
-import CartItem from "../CartItem/CartItem"
+import { CarritoContext } from "../../context/carritoContext";
+import "./Cart.css";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import CartItem from "../CartItem/CartItem";
 
 const Cart = () => {
-    const { carrito, vaciarCarrito, total, actualizarCantidad } = useContext(CarritoContext);
+    const { carrito, vaciarCarrito } = useContext(CarritoContext);
 
     const calcularCantidadTotal = () => {
         let cantidadTotal = 0;
@@ -15,36 +15,49 @@ const Cart = () => {
         return cantidadTotal;
     };
 
+    const calcularTotal = () => {
+        let total = 0;
+        carrito.forEach((producto) => {
+            total += producto.item.precio * producto.cantidad;
+        });
+        return total;
+    };
+
     if (calcularCantidadTotal() === 0) {
         return (
             <div className="carritoVacioCont">
-                <h2 className="NoProductos"> No hay productos en el carrito</h2>
-                <Link to='/'> <button className="btnVerProductos">Ver Productos</button> </Link>
+                <h2 className="NoProductos">No hay productos en el carrito</h2>
+                <Link to="/">
+                    <button className="btnVerProductos">Ver Productos</button>
+                </Link>
             </div>
-        )
+        );
     }
+
     return (
         <>
             <h2>Verifica tu orden</h2>
             <div className="carritoVisual">
                 <div className="firstBodyConfirm">
-                    {carrito.map(producto => (
-                        <CartItem
-                            key={producto.item.id}
-                            producto={producto}
-                            actualizarCantidad={actualizarCantidad} // Pasar la función actualizarCantidad
-                        />
+                    {carrito.map((producto) => (
+                        <CartItem key={producto.item.id} producto={producto} />
                     ))}
                 </div>
                 <div className="secondBodyConfirm">
-                    <h4 className="total">Total: s/{total} </h4>
-                    <h4 className="cantidadTotal">Número de productos: {calcularCantidadTotal()} </h4>
-                    <button onClick={() => vaciarCarrito()} className="btnVaciaryFin"> Vaciar carrito </button>
-                    <Link to='/checkout'> <button className="btnVaciaryFin">Finalizar Compra</button> </Link>
+                    <h4 className="total">Total: s/{calcularTotal()}</h4>
+                    <h4 className="cantidadTotal">
+                        Número de productos: {calcularCantidadTotal()}
+                    </h4>
+                    <button onClick={() => vaciarCarrito()} className="btnVaciaryFin">
+                        Vaciar carrito
+                    </button>
+                    <Link to="/checkout">
+                        <button className="btnVaciaryFin">Finalizar Compra</button>
+                    </Link>
                 </div>
             </div>
         </>
-    )
-}
+    );
+};
 
 export default Cart;
--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,9 +1,9 @@
-import { useContext, useState } from "react"
-import './CartItem.css'
+import { useContext, useState } from "react";
+import "./CartItem.css";
 import { CarritoContext } from "../../context/carritoContext";
 
-const CartItem = ({ producto, actualizarCantidad }) => { // Agregar actualizarCantidad como prop
-  const { eliminarProducto } = useContext(CarritoContext);
+const CartItem = ({ producto }) => {
+  const { eliminarProducto, actualizarCantidad } = useContext(CarritoContext);
   const { item, cantidad } = producto;
   const [cantidadProducto, setCantidadProducto] = useState(cantidad);
   const [precio, setPrecio] = useState(item.precio * cantidadProducto);
@@ -11,14 +11,14 @@ const CartItem = ({ producto, actualizarCantidad }) => { // Agregar actualizarCa
   const aumentarCantidad = () => {
     setCantidadProducto(cantidadProducto + 1);
     setPrecio(item.precio * (cantidadProducto + 1));
-    actualizarCantidad(item.id, cantidadProducto + 1); // Actualizar cantidad en el carrito
+    actualizarCantidad(item.id, cantidadProducto + 1);
   };
 
   const disminuirCantidad = () => {
     if (cantidadProducto > 1) {
       setCantidadProducto(cantidadProducto - 1);
       setPrecio(item.precio * (cantidadProducto - 1));
-      actualizarCantidad(item.id, cantidadProducto - 1); // Actualizar cantidad en el carrito
+      actualizarCantidad(item.id, cantidadProducto - 1);
     }
   };
 
@@ -27,15 +27,15 @@ const CartItem = ({ producto, actualizarCantidad }) => { // Agregar actualizarCa
       <h3>{item.nombre}</h3>
       <p>
         Cantidad: {cantidadProducto}
-        <button onClick={disminuirCantidad} className="btnqnt"> - </button>
-        <button onClick={aumentarCantidad} className="btnqnt"> + </button>
+        <button onClick={disminuirCantidad} className="btnqnt">-</button>
+        <button onClick={aumentarCantidad} className="btnqnt">+</button>
       </p>
       <p>Precio unitario: s/{item.precio}</p>
       <p>Precio total: s/{precio}</p>
       <button onClick={() => eliminarProducto(item.id)} className="btnEliminarProd">Eliminar</button>
       <hr />
     </div>
-  )
-}
+  );
+};
 
 export default CartItem;

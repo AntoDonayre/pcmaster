893d6958c4a122fdc620dc4bd886f1a7e8c6f8ec
Anthony Donayre <e.a.donayre95@gmail.com>
2023-06-17 09:49:56 -0500

    esttilos btn contadores

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,35 +1,37 @@
 import { useContext, useState } from "react"
 import './CartItem.css'
+import { Link } from "react-router-dom";
 import { CarritoContext } from "../../context/carritoContext";
 
 const CartItem = ({ item, cantidad }) => {
-    const { eliminarProducto } = useContext(CarritoContext);
-    const [cantidadProducto, setCantidadProducto] = useState(cantidad);
-    const [precio, setPrecio] = useState(item.precio * cantidadProducto);
+  const { eliminarProducto, vaciarCarrito } = useContext(CarritoContext);
+  const [cantidadProducto, setCantidadProducto] = useState(cantidad);
+  const [precio, setPrecio] = useState(item.precio * cantidadProducto);
 
-    const aumentarCantidad = () => {
-        setCantidadProducto(cantidadProducto + 1);
-        setPrecio(item.precio * (cantidadProducto + 1));
-    };
+  const aumentarCantidad = () => {
+    setCantidadProducto(cantidadProducto + 1);
+    setPrecio(item.precio * (cantidadProducto + 1));
+  };
 
-    const disminuirCantidad = () => {
-        if (cantidadProducto > 1) {
-            setCantidadProducto(cantidadProducto - 1);
-            setPrecio(item.precio * (cantidadProducto - 1));
-        }
-    };
+  const disminuirCantidad = () => {
+    if (cantidadProducto > 1) {
+      setCantidadProducto(cantidadProducto - 1);
+      setPrecio(item.precio * (cantidadProducto - 1));
+    }
+  };
 
-    return (
-        <div className="itemProdContainer">
-            <h3>{item.nombre}</h3>
-            <p>Cantidad: {cantidadProducto}</p>
-            <p>Precio unitario: s/{item.precio}</p>
-            <p>Precio total: s/{precio}</p>
-            <button onClick={disminuirCantidad} className="btnEliminarProd"> - </button>
-            <button onClick={aumentarCantidad} className="btnEliminarProd"> + </button>
-            <button onClick={() => eliminarProducto(item.id)} className="btnEliminarProd">Eliminar</button>
-        </div>
-    )
+  return (
+    <div className="itemProdContainer">
+      <h3>{item.nombre}</h3>
+      <p>Cantidad: {cantidadProducto}
+        <button onClick={disminuirCantidad} className="btnqnt"> - </button>
+        <button onClick={aumentarCantidad} className="btnqnt"> + </button>
+      </p>
+      <p>Precio unitario: s/{item.precio}</p>
+      <p>Precio total: s/{precio}</p>
+      <button onClick={() => eliminarProducto(item.id)} className="btnEliminarProd">Eliminar</button>
+    </div>
+  )
 }
 
-export default CartItem
+export default CartItem;
